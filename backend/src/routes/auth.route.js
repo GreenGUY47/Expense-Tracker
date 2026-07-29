6825b04022f0bf1074e7d35f7ed2f5d9e9d5961a
M.Azam <thegreenguy48@gmail.com>
2026-07-29 13:10:42 +0500

    cleanup

--- backend/src/routes/auth.route.js
+++
@@ -1,8 +0,0 @@
-import register from "../controllers/register.controller.js";
-import { Router } from "express";
-import validate from "../middleware/validate.middleware.js";
-import { registerSchema } from "../validators/auth.validator.js";
-
-const router = Router();
-
-router.post("/api/auth", validate(registerSchema), register);
